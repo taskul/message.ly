e1f074da2339cb7ae99b13b0b13e7136b5e17a14
tastasterone <tastankulmeshkenov@gmail.com>
2023-04-23 21:52:34 -0500

    first commit

---
+++ models/user.js
@@ -0,0 +1,74 @@
+/** User class for message.ly */
+const db = require('../db');
+const bcrypt = require('bcrypt');
+const {BCRYPT_WORK_FACTOR} = require('../config')
+
+
+/** User of the site. */
+
+class User {
+
+  /** register new user -- returns
+   *    {username, password, first_name, last_name, phone}
+   */
+
+  static async register({username, password, first_name, last_name, phone}) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
+    const joinAtDate = Date().now()
+    console.log(joinAtDate)
+    // const results = await db.query(
+    //   `INSERT INTO users (username, password, first_name, last_name, 
+    //     phone, join_at, last_login_at) 
+    //     VALUES ($1, $2, $3, $4, $5, $6, $7)
+    //     RETURNING username, last_login_at`,
+    //     [username, hashedPassword, first_name, last_name, phone]
+    // )
+  }
+
+  /** Authenticate: is this username/password valid? Returns boolean. */
+
+  static async authenticate(username, password) { }
+
+  /** Update last_login_at for user */
+
+  static async updateLoginTimestamp(username) { }
+
+  /** All: basic info on all users:
+   * [{username, first_name, last_name, phone}, ...] */
+
+  static async all() { }
+
+  /** Get: get user by username
+   *
+   * returns {username,
+   *          first_name,
+   *          last_name,
+   *          phone,
+   *          join_at,
+   *          last_login_at } */
+
+  static async get(username) { }
+
+  /** Return messages from this user.
+   *
+   * [{id, to_user, body, sent_at, read_at}]
+   *
+   * where to_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesFrom(username) { }
+
+  /** Return messages to this user.
+   *
+   * [{id, from_user, body, sent_at, read_at}]
+   *
+   * where from_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesTo(username) { }
+}
+
+
+module.exports = User;
